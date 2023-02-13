601b84f498f086f296b530707b6f758681511faf
BatVanko <71530199+BatVanko@users.noreply.github.com>
2023-02-13 23:15:53 +0200

    Add files via upload

---
+++ ex_3_payers_and_monsters/hero.py
@@ -0,0 +1,13 @@
+class Hero:
+
+    def __init__(self,username, level):
+        self.level = level
+        self.username = username
+    def __str__(self):
+        return f"{self.username} of type {__class__.__name__} has level {self.level}"
+
+
+
+
+
+
